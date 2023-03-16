7f6254f6679e5d52e2a3c9b199d1ee049db59416
DoHung <dohungdvh@gmail.com>
2023-03-16 17:40:34 +0700

    css input login form

---
+++ components/Input.tsx
@@ -0,0 +1,24 @@
+const Input = () => {
+  return (
+    <div>
+      <input
+        className="block
+        rounded-md
+        px-6
+        pt-6
+        pb-1
+        w-full
+        text-md
+      text-white
+      bg-neutral-700
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        peer
+        invalid:border-b-1s"
+      />
+    </div>
+  );
+};
+
+export default Input;
