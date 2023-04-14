04adbdde71f6298bf6afd9172720237631b5b788
DoHung <dohungdvh@gmail.com>
2023-04-14 17:33:37 +0700

    #billboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import useCurrentUser from "../hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,6 +23,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   );
 }
