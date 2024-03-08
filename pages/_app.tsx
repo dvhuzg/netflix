38c18e8365b4f9e87fa99b9fed4c516d545f5576
dvhuzg <dohungdvh@gmail.com>
2024-03-08 17:40:40 +0700

    fix error nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,15 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
-import React from "react";
 
-const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  const MainComponent = Component as any;
   return (
     <SessionProvider session={session}>
-      {/*@ts-expect-error */}
-      <Component {...pageProps} />
+      <MainComponent {...pageProps} />
     </SessionProvider>
   );
-};
-export default App;
+}
